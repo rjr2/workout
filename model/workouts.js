0f646cd07b6e9ff62fb84466a59b79546cac8777
RJR <80534043+rjr2@users.noreply.github.com>
2021-10-10 12:40:56 -0400

    changed schema to reflect seed.js layout

--- model/workouts.js
+++ model/workouts.js
@@ -3,36 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    excersizeType: {
-      type: String,
-      trim: true,
-    },
-    excersizeName: { 
-        type: String,
-        trim: true,
-        required: "Enter a name for workout",
-    },
-    Distance: {
-        type: Number,
-        required: "Enter a distance",
-    },
-    Weight: {
-      type: Number,
-      trim: true,
-    },
-    Sets:{
-      type: Number,
-      trim: true,
-    },
-    Reps:{
-      type: Number,
-      trim: true,
-    },
-    Duration: {
-      type: Number,
-      trim: true,
-    },
-   
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
