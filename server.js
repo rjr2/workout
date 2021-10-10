c0bf4181bfba14b7334f29f657f3805ce46ebf87
RJR <80534043+rjr2@users.noreply.github.com>
2021-10-10 13:04:39 -0400

    seed file adjusted

--- server.js
+++ server.js
@@ -3,27 +3,20 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3002;
 
-
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-
 mongoose
-.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-}).then(() => {
-  const app = express();
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const app = express();
 
-  app.use(express.static("public"));
-  app.use(require("./routes/index.js"));
+    app.use(express.json());
+    app.use(express.static("public"));
+    app.use(require("./routes/index.js"));
 
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    app.listen(PORT, () => {
+      console.log(`App running on port ${PORT}!`);
+    });
   });
-});
-
-// routes
-
